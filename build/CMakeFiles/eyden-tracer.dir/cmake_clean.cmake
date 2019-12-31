3159e80df1b0b1ebf0e7d7c49fb4db3f62464415
aronzek <aronjinga@gmail.com>
2019-12-31 10:10:09 +0100

    Add files via upload

---
+++ build/CMakeFiles/eyden-tracer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eyden-tracer.dir/src/Scene.cpp.obj"
+  "CMakeFiles/eyden-tracer.dir/src/main.cpp.obj"
+  "bin/eyden-tracer.exe"
+  "bin/eyden-tracer.exe.manifest"
+  "bin/eyden-tracer.pdb"
+  "bin/libeyden-tracer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eyden-tracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
